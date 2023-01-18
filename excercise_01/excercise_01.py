277bd2dda9e13d3e4ffab0862b8d3923c3a497a2
fadin47 <vfadin47@gmail.com>
2023-01-17 21:10:34 -0500

    Create excercise_01.py

---
+++ excercise_01/excercise_01.py
@@ -0,0 +1,12 @@
+x = input('enter a grade from 0 to 100: ')
+a = int(x)
+if a >= 90:
+  print('A')
+elif a >=80:
+  print('B')
+elif a >=70:
+  print('C')
+elif a >=60:
+  print('D')
+elif a >=50:
+  print('F')
